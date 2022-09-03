8dd4bdcaa82be0a1041a19f7b236bc483a39d291
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2022-09-03 21:48:28 +0500

    add transition-group

--- src/components/AppTodoGroup.vue
+++ src/components/AppTodoGroup.vue
@@ -1,5 +1,15 @@
 <template>
-  <ul class="w-full flex flex-col space-y-3">
+  <transition-group
+    tag="ul"
+    class="relative w-full flex flex-col gap-y-1.5"
+    enter-from-class="opacity-0"
+    enter-active-class="transition-opacity duration-500"
+    enter-to-class="opacity-100"
+    leave-from-class="opacity-100"
+    leave-active-class="absolute -z-10 transition-opacity duration-500"
+    leave-to-class="opacity-0"
+    move-class="transition-all duration-200"
+  >
     <li
       v-for="todo in todos"
       :key="todo.id"
@@ -7,7 +17,7 @@
     >
       <app-todo :id="todo.id" :text="todo.text" :completed="todo.completed" />
     </li>
-  </ul>
+  </transition-group>
 </template>
 
 <script lang="ts">
